4911b9fcb66676be691425899054a90f5993fe27
MarcoHannemann <marco.hannemann@mailbox.org>
2023-05-05 10:16:59 +0200

    remove tests of geopackage files intest_waterbalance.py, as they are not needed

--- sbat/tests/test_waterbalance.py
+++ sbat/tests/test_waterbalance.py
@@ -26,25 +26,3 @@ class TestConfig1:
         result = model_config1.sections_meta
         result["decade"] = result["decade"].astype(int)
         pd.testing.assert_frame_equal(expected, result)
-
-    def test_sections_streamlines(self, model_config1):
-        expected_gdf = gpd.read_file(
-            "data/example1/sections_streamlines.gpkg", index_col=0
-        )
-        expected = pd.DataFrame(expected_gdf)
-        expected.drop(columns=["id", "geometry"], inplace=True)
-        result = model_config1.gdf_network_map
-        result = pd.DataFrame(result)
-        result.drop(columns=["id", "geometry"], inplace=True)
-        pd.testing.assert_frame_equal(expected, result)
-
-    def test_sections_subbasins(self, model_config1):
-        expected_gdf = gpd.read_file(
-            "data/example1/sections_subbasins.gpkg", index_col=0
-        )
-        expected = pd.DataFrame(expected_gdf)
-        expected.drop(columns=["geometry"], inplace=True)
-        result = model_config1.section_basins
-        result = pd.DataFrame(result)
-        result.drop(columns=["geometry"], inplace=True)
-        pd.testing.assert_frame_equal(expected, result)
