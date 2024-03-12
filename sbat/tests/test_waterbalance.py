67239646617c8b997daf7e963448e4d763e4c9c8
Erik Nixdorf <erik.nixdorf@bgr.de>
2024-03-12 13:55:45 +0100

    fix tests

--- sbat/tests/test_waterbalance.py
+++ sbat/tests/test_waterbalance.py
@@ -4,14 +4,16 @@ from pathlib import Path
 
 class TestConfig1:
     def test_updated_gauges_meta(self, model_config1):
-        expected = [0.46237469, 0.127907  , 0.46237469, 0.127907  ]
-        result = model_config1.gauges_meta[['balance[m³/s]']].dropna().values
+        expected = [0.40328308, 0.40328308, 0.14005813, 0.14005813]
+        result = model_config1.gauges_meta[['balance[m³/s]']].sort_index().dropna().values.flatten()
         np.testing.assert_almost_equal(result, expected, decimal=6)
 
     def test_sections_meta(self, model_config1):
-        expected = pd.read_csv(Path(Path(__file__).parents[0],"data/example1/sections_meta.csv"), index_col=0)
+        expected = pd.read_csv(Path(Path(__file__).parents[0],"data/example1/sections_meta.csv"), index_col=[0])
+        expected = expected.reset_index(drop=True).set_index(['date', 'sample_id', 'downstream_point', 'decade'])
         result = model_config1.sections_meta
         result["decade"] = result["decade"].astype(np.int64)
+        result = result.set_index(['date', 'sample_id', 'downstream_point', 'decade'])
         pd.testing.assert_frame_equal(expected, result)
 
 
@@ -24,6 +26,7 @@ class TestConfig3:
         pd.testing.assert_series_equal(expected, result)
 
     def test_updated_gauges_meta_nans(self, model_config3):
+        balance_col = 'balance[m³/s]'
         expected = pd.Series(
             index=['doberburg',
                      'doberburg_muehle_up',
@@ -48,18 +51,20 @@ class TestConfig3:
                      'schoenfeld',
                      'schoenfeld',
                      'schoeps'],
-            name="balance",
+            name=balance_col,
         )
         expected.index.name = "gauge"
-        single_indexed = model_config3.gauges_meta[['balance[m³/s]']].sort_index().reset_index(
+        single_indexed = model_config3.gauges_meta[[balance_col]].sort_index().reset_index(
                                 level=1,
                                 drop=True,
                                 )
-        result = single_indexed['balance[m³/s]'][single_indexed['balance[m³/s]'].isnull()]
+        result = single_indexed[balance_col][single_indexed[balance_col].isnull()]
         pd.testing.assert_series_equal(expected, result)
 
     def test_sections_meta(self, model_config3):
         expected = pd.read_csv(Path(Path(__file__).parents[0],"data/example3/sections_meta.csv"), index_col=[0])
         expected = expected.reset_index(drop=True).set_index(['date', 'sample_id', 'downstream_point', 'decade'])
-        result = model_config3.sections_meta.set_index(['date', 'sample_id', 'downstream_point', 'decade'])
+        result = model_config3.sections_meta
+        result["decade"] = result["decade"].astype(np.int64)
+        result = result.set_index(['date', 'sample_id', 'downstream_point', 'decade'])
         pd.testing.assert_frame_equal(expected.sort_index(), result.sort_index())
